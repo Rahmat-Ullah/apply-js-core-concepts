6aa06d8677bfbcaa97c07b63592cf66dc5daf411
Rahmat Ullah <rahmatbd1533@gmail.com>
2022-09-15 20:42:00 +0600

    odd number of an array added

--- find-oddSum.js
+++ find-oddSum.js
@@ -1,6 +1,6 @@
 function getSum(numbers){
     let sum = 0;
-    for(i = 0; i < numbers.length; i++){
+    for(let i = 0; i < numbers.length; i++){
         const index = i;
         const element = numbers[index];
         sum = sum + element;
@@ -9,5 +9,22 @@ function getSum(numbers){
     return sum;
 }
 
+function getOddNumbersOfAnArray(numbers){
+    const oddNumbers = [];
+    for(let i = 0; i < numbers.length; i++){
+        const index = 0;
+        const element = numbers[index];
+        if(element % 2 !== 0){
+            console.log(index, element);
+            oddNumbers.push(element);
+        }
+    }
+    return oddNumbers;
+}
+
 const myNumbers = [23, 12, 24, 45, 56, 67,78];
+const oddNumbers = getOddNumbersOfAnArray(myNumbers);
+console.log(oddNumbers);
+const oddNumbersSum = getOddNumbersOfAnArray(oddNumbers);
+console.log('odd numbers sum: ', oddNumbers);
 getSum(myNumbers);
